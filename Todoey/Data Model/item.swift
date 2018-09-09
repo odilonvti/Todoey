482ca327224839d0c278ecb33f72293f61cd27d6
Odilon Pereira <novomac@macbook-pro-de-odilon.local>
2018-09-09 16:08:00 -0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Odilon Pereira on 9/8/18.
+//  Copyright © 2018 odilon. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
